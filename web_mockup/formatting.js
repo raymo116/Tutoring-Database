8dcba610c61c39c041cb9c3e6353a73f4cf5bb11
raymo116 <raymo116@mail.chapman.edu>
2020-04-30 00:36:34 -0400

    simplified table

--- web_mockup/formatting.js
+++ web_mockup/formatting.js
@@ -1,9 +1,8 @@
-function fnFillData(lstData, lstHeader, lstFields, targetID) {
-    if (arguments.length == 2) {
-        targetID = arguments[1][2];
-        lstFields = arguments[1][1];
-        lstHeader = arguments[1][0];
-    } else if (arguments.length != 4) throw "Wrong number of arguments";
+function fnFillData(lstData, lstColNames, ...rest) {
+    for (var i = 0; i < lstColNames.length; i++)
+        lstColNames[i] = lstColNames[i]["name"];
+
+    var targetID = rest[0];
 
     // Find the source
     var grid = $('#'+targetID)[0];
@@ -14,10 +13,10 @@ function fnFillData(lstData, lstHeader, lstFields, targetID) {
     var header = document.createElement('tr');
     header.classList.add('header');
 
-    for (var i = 0; i < lstHeader.length; i++) {
+    for (var i = 0; i < lstColNames.length; i++) {
         var cell = document.createElement('th');
         cell.classList.add('header');
-        cell.innerHTML = lstHeader[i];
+        cell.innerHTML = lstColNames[i];
         header.appendChild(cell);
     }
     table.appendChild(header);
@@ -28,11 +27,11 @@ function fnFillData(lstData, lstHeader, lstFields, targetID) {
         var row = document.createElement('tr');
         row.classList.add('row'+(i%2+1));
 
-        for (var k = 0; k < lstFields.length; k++) {
+        for (var k = 0; k < lstColNames.length; k++) {
             var cell = document.createElement('th');
-            if (lstData[i][lstFields[k]] == null)
-                lstData[i][lstFields[k]] = '-';
-            cell.innerHTML = lstData[i][lstFields[k]];
+            if (lstData[i][lstColNames[k]] == null)
+                lstData[i][lstColNames[k]] = '-';
+            cell.innerHTML = lstData[i][lstColNames[k]];
             row.appendChild(cell);
         }
 
